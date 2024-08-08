ec5ec3348e9963c0192a0f6142f237ea80ee39f3
l1kholetov <likholetov.vova@bk.ru>
2024-08-08 17:24:46 +0300

    implementing a fetch request on the API on the Marvel Developer Portal

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
+import MarvelService from './services/MarvelService';
+
 import './style/style.scss';
 
-ReactDOM.render(
+const marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
---
+++ src/services/MarvelService.js
@@ -0,0 +1,24 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=53ec7707695c342bb1dc5fd87035cbcb';
+
+    getResourse = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+}
+
+export default MarvelService;
